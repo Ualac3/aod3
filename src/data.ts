8975dcb0db982473b76fcbb77f4c95c40ed583be
Ualac3 <michaelray98@hotmail.co.uk>
2025-09-08 17:17:26 +0100

    ffd

--- src/data.ts
+++ src/data.ts
@@ -15,7 +15,7 @@ export const minions = {
             "Break through its limbs if you must",
             "Get away from its heart",
             "It's a trap",
-            "Nothing can withstand that amouBreasnt",
+            "Nothing can withstand that amount",
             "Smash your way out",
             "Break through the arms",
             "Destroy the arms",
